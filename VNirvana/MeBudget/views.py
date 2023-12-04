5e00f38e5fd0edefad9420f06864979a2fd8d0a6
ilkin alizade <74135917+TheIlkinAlizade@users.noreply.github.com>
2023-12-05 01:03:21 +0400

    Full version

--- VNirvana/MeBudget/views.py
+++ VNirvana/MeBudget/views.py
@@ -1,53 +1,151 @@
 import random
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django import forms
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from .models import MyModel, User, Expense, Earning, Budget
+from .forms import MyForm, UserForm, ExpenseForm, EarningForm, BudgetForm
+
 
 import re
 
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
 
+
 # Create your views here.
 
-
 def index(request):
-    motivation = ""
+    return render(request, "MeBudget/index.html")
+
+
+
+def profile(request):
+    id = request.session.get('user_id')
+
+    try:
+        user = User.objects.get(id=id)
+    except User.DoesNotExist:
+        # Redirect to a custom URL if the user is not found
+        return redirect(login_view)
+
     motivation = get_random_motivation_messages()
 
-    return render(request, "MeBudget/index.html", {"text": motivation})
+    modelBudget = Budget.objects.filter(user_id=id)
+    modelExpense = Expense.objects.filter(user_id=id)
+    modelEarning = Earning.objects.filter(user_id=id)
+    
+    budgetform = BudgetForm(request.POST)
+    expenseform = ExpenseForm(request.POST)
+    earningform = EarningForm(request.POST)
 
 
-
-def login(request):
-    return render(request, "Mebudget/login.html")
-'''
-
-def save_entry(title, content):
-    """
-    Saves an encyclopedia entry, given its title and Markdown
-    content. If an existing entry with the same title already exists,
-    it is replaced.
-    """
-    filename = f"datas/{title}.md"
-    if default_storage.exists(filename):
-        default_storage.delete(filename)
-    default_storage.save(filename, ContentFile(content))
-
-   if request.method == "POST":
-        entry_title = request.POST['entry_title']
-        textarea = request.POST['new_entry']
-        if util.get_entry(entry_title):
-            return render(request, "encyclopedia/create_new_page.html",{"text": error_message}) 
+    if request.method == "POST":
+        if expenseform.is_valid():
+            expense_instance = expenseform.save(commit=False)
+            expense_instance.user_id = id  # Assign the user ID to the user_id field
+            expense_instance.save()
+            modelExpense = Expense.objects.filter(user_id=id)
+            expenseform = ExpenseForm()
         else:
-            util.save_entry(entry_title, textarea)
-            return HttpResponseRedirect(reverse('entry', kwargs={'name': entry_title}))
-    else:
-        return render(request, "encyclopedia/create_new_page.html",{"text": ""})
-'''
+            expenseform = ExpenseForm()
 
+        if earningform.is_valid():
+            earning_instance = earningform.save(commit=False)
+            earning_instance.user_id = id  # Assign the user ID to the user_id field
+            earning_instance.save()
+            modelEarning = Earning.objects.filter(user_id=id)
+            earningform = EarningForm() 
+        else:
+            earningform = EarningForm() 
+
+        if budgetform.is_valid():
+            budget_instance = budgetform.save(commit=False)
+            budget_instance.user_id = id  # Assign the user ID to the user_id field
+            budget_instance.save()
+            modelBudget = Budget.objects.filter(user_id=id)
+            budgetform = BudgetForm()  
+        else:
+            budgetform = BudgetForm()  
+
+    user_budget_amount_list = modelBudget.values_list('total_amount', flat=True)
+    user_expense_amounts_list = modelExpense.values_list('user_amount', flat=True)
+    user_expense_product_list = modelExpense.values_list('product_title', flat=True)
+    user_earning_amounts_list = modelEarning.values_list('earning_amount', flat=True)
+    user_budget_amount = sum(user_budget_amount_list)
+    user_expense_amounts = sum(user_expense_amounts_list)
+    user_earning_amounts = sum(user_earning_amounts_list)
+    
+    balance = user_budget_amount + user_earning_amounts - user_expense_amounts
+    if len(user_budget_amount_list)-1 >= 0:
+        budget = user_budget_amount_list[len(user_budget_amount_list)-1]
+    else:
+        budget = 0
+
+    username = User.objects.get(id=id).username
+
+
+    return render(request, "MeBudget/profile.html", {"text": motivation, "expenseform": expenseform, "earningform": earningform, "budgetform": budgetform,
+    "budget": budget, "expenses": user_expense_amounts, "earnings": user_earning_amounts, "balance": balance, "id": id, "products": modelExpense,
+    "user": username
+    })
+
+
+def register_view(request):
+    model = User.objects.all()
+    form = UserForm(request.POST)
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            username = request.POST.dict().get("username")
+            password = request.POST.dict().get("password")
+            if not User.objects.filter(username = username).exists():
+                form.save()
+                form = UserForm()  
+                request.method = "GET"
+            else:
+                error_msg = "--User already registered--"
+                return render(request, "Mebudget/register.html", {'form': form, 'error_msg': error_msg})
+            id = User.objects.get(username=username).id
+            request.session['user_id'] = id
+            form = UserForm()  
+            return redirect(profile)
+        else:
+            form = UserForm()
+    error_msg = ""
+    return render(request, "Mebudget/register.html", {'form': form, 'error_msg': error_msg})
+
+
+def login_view(request):
+    model = User.objects.all()
+    form = UserForm(request.POST)
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            username = request.POST.dict().get("username")
+            password = request.POST.dict().get("password")
+            form = UserForm() 
+            if User.objects.filter(username = username).exists() and User.objects.filter(password = password).exists():
+                request.method = "GET"
+                request.session['user_id'] = User.objects.get(username=username).id
+                return redirect(profile)
+            else:
+                error_msg = "--Please enter valid info--"
+                return render(request, "Mebudget/login.html", {'form': form, 'error_msg': error_msg})
+        else:
+            form = UserForm()
+    error_msg = ""
+    return render(request, "Mebudget/login.html", {'form': form, 'error_msg': error_msg})
+
+def logout_view(request):
+    request.session['user_id'] = None
+    logout(request)
+    return redirect('index')
+
+def Listofname(request):
+   model = User.objects.all()
+   return render(request, 'MeBudget/listofnames.html', {'Data' : model})
 
 def get_random_motivation_messages():
     motivation_messages = [
@@ -72,8 +170,20 @@ def get_random_motivation_messages():
         "Success is not in what you have, but who you are. - Bo Bennett",
         "The best way to predict the future is to create it. - Peter Drucker",
         "Don't be pushed around by the fears in your mind. Be led by the dreams in your heart. - Roy T. Bennett",
-        # ... Eklemeleri buraya yapabilirsiniz
+        # ... You can add in here
     ]
 
     selected_message = random.choice(motivation_messages)
     return selected_message
+
+
+def editProduct(id):
+    return 0
+
+    
+def deleteProduct(id):
+    return 0
+
+
+#cs50 session stuff / user page authoritize / take data with same id / return same ids (for expense list)
+#find a way to make to do app with register login
